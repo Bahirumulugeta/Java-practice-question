e4afa2e8c60c15a41151ccba4abec58c9d3d9f44
bahiru <bahirumulugeta1@gmail.com>
2022-06-08 16:38:17 +0200

    n has k factor

--- newproject/src/App.java
+++ newproject/src/App.java
@@ -1,46 +1,20 @@
 package newproject.src;
 public class App {
     public static void main(String... args){
-        int array[]={2,3,5,4,6,7};
-        int ret=isComplate(array);
-        if(ret==1){
-            System.out.print("it is complate");
+        boolean ret=hasKSmallFactors(7,30);
+        if(ret){
+            System.out.print("n has k factors");
         }
         else{
-            System.out.print("it is not complate");
+            System.out.print("n has not k factors");
         }
     }
-    public static int isComplate(int[] args){
-        int inc=0;int min=args[0];int max=args[0];
-        for(int i=0;i<args.length;i++){
-            if(args[i]%2==0){
-                if(args[i]<min){
-                    min=args[i];
-                }
-                else if(args[i]>max){
-                    max=args[i];
-                }
-            }
-            else{
-                if(args[i]==min){
-                    min=args[i+1];
-                }
+    public static boolean hasKSmallFactors(int k, int n){
+        for(int i=2;i<k;i++){
+           if((n%i)==0 && (n/i)<k ){
+                return true;
             }
         }
-        int diff;int size=0;
-        if(min!=max){
-            for(int j=0;j<args.length;j++){
-                if(args[j]>min&&args[j]<max){
-                    size++;
-                }
-            }
-            diff=max-min;
-           if(diff-1==size){
-               return 1;
-           }
-           else return 0;
-        }
-        else
-           return 0;
+        return false;
     }
 }
