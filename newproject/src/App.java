4f3d47eb318ea89a6a5f548dd8b60cdb58308fd0
bahiru <bahirumulugeta1@gmail.com>
2022-06-15 16:02:42 +0200

    is meera array

--- newproject/src/App.java
+++ newproject/src/App.java
@@ -1,20 +1,45 @@
 package newproject.src;
+
 public class App {
-    public static void main(String... args){
-        boolean ret=hasKSmallFactors(7,30);
-        if(ret){
-            System.out.print("n has k factors");
-        }
-        else{
-            System.out.print("n has not k factors");
-        }
+    public static void main(String... args) {
+        int arr[] = { 6, 10, 1 };
+        System.out.println(isMeera(arr));
     }
-    public static boolean hasKSmallFactors(int k, int n){
-        for(int i=2;i<k;i++){
-           if((n%i)==0 && (n/i)<k ){
-                return true;
+
+    public static int isMeera(int[] arr) {
+        boolean hasPrime = false;
+        boolean hasZero = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                if (hasPrime)
+                    return 1;
+                else
+                    hasZero = true;
+            } else {
+                if (isPrime(arr[i]) == 1 && hasZero == true)
+                    return 1;
+                else if (isPrime(arr[i]) == 1 && hasZero == false)
+                    hasPrime = true;
+                else continue;
             }
         }
-        return false;
+        if (hasPrime == false && hasZero == false)
+            return 1;
+        else
+            return 0;
     }
+
+    public static int isPrime(int num) {
+        int total = 0;
+
+        for (int j = 1; j <= num; j++) {
+            if (num % j == 0)
+                total += 1;
+        }
+        if (total == 2)
+            return 1;
+        else
+            return 0;
+    }
+
 }
